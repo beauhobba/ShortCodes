1ce5098b86f9638e55ff36eebd368455999293f5
beau <hobbabeau@gmail.com>
2022-05-22 21:04:01 +1000

    Added: Arguments

---
+++ args/main.py
@@ -0,0 +1,13 @@
+
+def roll_call(name, age, phone):
+    print("name", name,"age",age,"phone",phone)
+    
+    
+student1 = ['beau', 20, '0438']
+student2 = {"name": "john", "age": 23, "phone": "0438"}
+
+
+roll_call('beau', 20, '0438')
+roll_call(*student1)
+roll_call(**student2)
+
